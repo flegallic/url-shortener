4339b3ea4b3d74c0be5aceb1d006d90bf58a671b
wzvd9898 <fabrice.legallic@orange.com>
2022-07-14 23:07:24 +0200

    bug fix update

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
-// event logging
-const morgan = require("morgan");
-app.use(morgan('combined'));
-
-// secure apps by setting various HTTP headers
-const helmet = require("helmet");
-app.use(helmet());
-
-// api access controls
-var cors = require('cors');
-const PORT = 80;
-const HOST = '0.0.0.0';
-var corsOptions = { origin: `http://${HOST}:${PORT}`};
-app.use(cors(corsOptions));
 var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//import routes
-var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
